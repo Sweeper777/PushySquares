abd93618e187c0b88795bb9e2e9e14c836281017
Sweeper777 <sumulang@gmail.com>
2017-08-30 08:13:41 +0100

    Initial Commit

---
+++ PushySquares/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PushySquares
+//
+//  Created by Mulang Su on 2017/08/30.
+//  Copyright © 2017年 Mulang Su. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
