2496d7bc96404c3f9743c73a7be0f2e89266ae01
Sweeper777 <sumulang@gmail.com>
2017-08-31 10:04:30 +0100

    improved Tile enum

--- PushySquares/Tile.swift
+++ PushySquares/Tile.swift
@@ -1,8 +1,15 @@
-import UIKit
-
 public enum Tile {
     case empty
     case void
     case wall
-    case square(UIColor)
+    case square(Color)
+    case spawn(Color)
+}
+
+public enum Color {
+    case color1
+    case color2
+    case color3
+    case color4
+    case grey
 }
