ae8d9ce035d5cac3b4f7ce06fc2869d7f85d14a6
Sweeper777 <sumulang@gmail.com>
2017-09-02 08:14:07 +0100

    gave player lives a lower limit

--- PushySquares/Player.swift
+++ PushySquares/Player.swift
@@ -1,6 +1,12 @@
 public class Player {
     public var turnsUntilNewSquare: Int
-    public var lives: Int
+    public var lives: Int {
+        didSet {
+            if lives < 0 {
+                lives = 0
+            }
+        }
+    }
     public let color: Color
     
     init(turnsUntilNewSquare: Int, lives: Int, color: Color) {
