6559e5fb8cf63fc2174d03926734a819afef9d8b
Sweeper777 <sumulang@gmail.com>
2017-09-02 08:15:03 +0100

    added game class

---
+++ PushySquares/Game.swift
@@ -0,0 +1,13 @@
+
+
+class Game {
+    public var board: Array2D<Tile>
+    public var spawnpoints: [Color: Position]
+    public var players = [Player]()
+    public var currentPlayer: Player {
+        return players[currentPlayerIndex]
+    }
+    
+    private var currentPlayerIndex = 0
+    
+}
