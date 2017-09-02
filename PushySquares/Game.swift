e73fb0208c61b9e95dc621c8882188637b471854
Sweeper777 <sumulang@gmail.com>
2017-09-02 08:16:14 +0100

    added initializer in Game

--- PushySquares/Game.swift
+++ PushySquares/Game.swift
@@ -10,6 +10,28 @@ class Game {
     
     private var currentPlayerIndex = 0
     
+    public init(map: Map, playerCount: Int, lives: Int = 5) {
+        self.board = map.board
+        self.spawnpoints = map.spawnpoints
+        switch playerCount {
+        case 4:
+            players.append(Player(turnsUntilNewSquare: playerCount + 1, lives: lives, color: .color4))
+            spawnNewSquare(color: .color4)
+            fallthrough
+        case 3:
+            players.append(Player(turnsUntilNewSquare: playerCount + 1, lives: lives, color: .color2))
+            spawnNewSquare(color: .color2)
+            fallthrough
+        case 2:
+            players.append(Player(turnsUntilNewSquare: playerCount + 1, lives: lives, color: .color1))
+            players.append(Player(turnsUntilNewSquare: playerCount + 1, lives: lives, color: .color3))
+            spawnNewSquare(color: .color1)
+            spawnNewSquare(color: .color3)
+        default:
+            fatalError()
+        }
+    }
+    
     private func spawnNewSquare(color: Color) {
         board[spawnpoints[color]!] = .square(color)
     }
