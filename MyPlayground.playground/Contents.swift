cd8fae5ce2448d543b6d4e799768767af1df52d1
Sweeper777 <sumulang@gmail.com>
2017-09-04 07:10:34 +0100

    added printBoard method

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,4 +1,31 @@
 import PushySquaresModel
 import UIKit
 
-let a: Tile = .wall
+func printBoard(_ board: Array2D<Tile>) {
+    for y in 0..<board.columns {
+        for x in 0..<board.rows {
+            switch board[x, y] {
+            case .empty:
+                print("⬜️", separator: "", terminator: "")
+            case .wall:
+                print("🔲", separator: "", terminator: "")
+            case .void:
+                print("▫️", separator: "", terminator: "")
+            case .square(let color):
+                switch color {
+                case .color1:
+                    print("🚹", separator: "", terminator: "")
+                case .color2:
+                    print("🚺", separator: "", terminator: "")
+                case .color3:
+                    print("🚼", separator: "", terminator: "")
+                case .color4:
+                    print("❇️", separator: "", terminator: "")
+                case .grey:
+                    print("ℹ️", separator: "", terminator: "")
+                }
+            }
+        }
+        print("")
+    }
+}
