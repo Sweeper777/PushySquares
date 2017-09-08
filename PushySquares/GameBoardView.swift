916502612877ff323edc90f5ae5c6f3800cfe526
Sweeper777 <sumulang@gmail.com>
2017-09-08 07:24:10 +0100

    added properties to GameBoardView

--- PushySquares/GameBoardView.swift
+++ PushySquares/GameBoardView.swift
@@ -3,4 +3,20 @@ import SwiftyUtils
 
 @IBDesignable
 class GameBoardView: UIView {
+    static let colorToUIColor: [Color: UIColor] = [
+        .color1: .red,
+        .color2: .blue,
+        .color3: .green,
+        .color4: .yellow,
+        .grey: .gray
+    ]
+    
+    static let borderSize: CGFloat = 8
+    
+    weak var game: Game? {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
 }
