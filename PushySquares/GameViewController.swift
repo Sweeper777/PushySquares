f82acd1b7836393160e3b503f15b1d440b5b6e61
Sweeper777 <sumulang@gmail.com>
2017-09-11 16:49:22 +0100

    conformed GameVC to game delegate

--- PushySquares/GameViewController.swift
+++ PushySquares/GameViewController.swift
@@ -1,12 +1,15 @@
 import UIKit
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController, GameDelegate {
 
     @IBOutlet var boardView: GameBoardView!
     let game = Game(map: .standard, playerCount: 4)
     
     override func viewDidLoad() {
         boardView.game = self.game
+    }
+    
+    func playerDidMakeMove(direction: Direction?, originalPositions: [Position], destroyedSquarePositions: [Position], greyedOutPositions: [Position], newSquareColor: Color?) {
     }
 }
 
