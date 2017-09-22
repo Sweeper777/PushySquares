87e24a23ffe410d31e364bfc6c610d586da587bb
Sweeper777 <sumulang@gmail.com>
2017-09-22 06:56:04 +0100

    changed formatting of players' lives

--- PushySquares/StatusBar.swift
+++ PushySquares/StatusBar.swift
@@ -17,18 +17,23 @@ class StatusBar: UIView {
     }
     
     func setLives(players: [Player]) {
-        let text = NSMutableAttributedString(string: "LIVES:\n")
-        let player1Lives = NSAttributedString(string: "♥︎\(players[0].lives) ", attributes: [NSForegroundColorAttributeName: GameBoardView.colorToUIColor[players[0].color]!])
+        let text = NSMutableAttributedString(string: "LIVES\n")
+        let player1Lives = NSAttributedString(string: "♥︎", attributes: [NSForegroundColorAttributeName: GameBoardView.colorToUIColor[players[0].color]!])
         text.append(player1Lives)
-        let player2Lives = NSAttributedString(string: "♥︎\(players[1].lives)", attributes: [NSForegroundColorAttributeName: GameBoardView.colorToUIColor[players[1].color]!])
+        text.append(NSAttributedString(string: "\(players[0].lives) "))
+        let player2Lives = NSAttributedString(string: "♥︎", attributes: [NSForegroundColorAttributeName: GameBoardView.colorToUIColor[players[1].color]!])
         text.append(player2Lives)
-        guard players.count > 2 else { return }
-        let player3Lives = NSAttributedString(string: "\n♥︎\(players[2].lives)", attributes: [NSForegroundColorAttributeName: GameBoardView.colorToUIColor[players[2].color]!])
-        text.append(player3Lives)
-        guard players.count > 3 else { return }
-        let player4Lives = NSAttributedString(string: " ♥︎\(players[3].lives)", attributes: [NSForegroundColorAttributeName: GameBoardView.colorToUIColor[players[3].color]!])
-        text.append(player4Lives)
-        lblLives.attributedText = text
+        text.append(NSAttributedString(string: "\(players[1].lives) "))
+        if players.count > 2 {
+            let player3Lives = NSAttributedString(string: "\n♥︎", attributes: [NSForegroundColorAttributeName: GameBoardView.colorToUIColor[players[2].color]!])
+            text.append(player3Lives)
+            text.append(NSAttributedString(string: "\(players[2].lives)"))
+        }
+        if players.count > 3 {
+            let player4Lives = NSAttributedString(string: " ♥︎", attributes: [NSForegroundColorAttributeName: GameBoardView.colorToUIColor[players[3].color]!])
+            text.append(player4Lives)
+            text.append(NSAttributedString(string: "\(players[3].lives)"))
+        }
     }
     
     override init(frame: CGRect) {
