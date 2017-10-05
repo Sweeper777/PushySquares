6f7ce989d713dda90f47a74bee2e40d70701bc8e
Sweeper777 <sumulang@gmail.com>
2017-10-05 07:11:06 +0100

    started writing repositionViews

--- PushySquares/MainMenuController.swift
+++ PushySquares/MainMenuController.swift
@@ -4,4 +4,10 @@ import SwiftyButton
 class MainMenuController: UIViewController {
     @IBOutlet var logo: UIImageView!
     
+    var viewsToBeRepositioned: [UIView] = []
+    
+    func repositionViews() {
+        viewsToBeRepositioned.forEach { $0.removeFromSuperview() }
+        viewsToBeRepositioned = []
+    }
 }
