ae336c80efee0784656af364cb10100d1883da84
Sweeper777 <sumulang@gmail.com>
2017-10-08 08:48:25 +0100

    started writing repositionViews

--- PushySquares/PlayerCountSelectorController.swift
+++ PushySquares/PlayerCountSelectorController.swift
@@ -3,4 +3,7 @@ import SwiftyButton
 
 class PlayerCountSelectorController: UIViewController {
     
+    func repositionViews() {
+        self.view.subviews.forEach { $0.removeFromSuperview() }
+    }
 }
