d727e44f92b3bb20cc47077b5930eed19d498e6b
Sweeper777 <sumulang@gmail.com>
2017-10-09 17:28:43 +0100

    calculated back button font size

--- PushySquares/PlayerCountSelectorController.swift
+++ PushySquares/PlayerCountSelectorController.swift
@@ -17,6 +17,7 @@ class PlayerCountSelectorController: UIViewController {
         let backButtonHeight = (view.height - 8) * backButtonWeight
         let backButtonWidth = 2.5 * backButtonHeight
         let backButton = PressableButton(frame: CGRect(x: backButtonX, y: backButtonY, width: backButtonWidth, height: backButtonHeight))
+        let fontSize = fontSizeThatFits(size: backButton.frame.size, text: "BACK", font: UIFont(name: "Chalkboard SE", size: 0)!) * 0.7
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
