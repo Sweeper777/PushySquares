b67b9c36c6e8cf41adaac137d3617ae084f8b626
Sweeper777 <sumulang@gmail.com>
2017-10-09 17:25:43 +0100

    called repositionViews appropriately

--- PushySquares/PlayerCountSelectorController.swift
+++ PushySquares/PlayerCountSelectorController.swift
@@ -5,5 +5,18 @@ class PlayerCountSelectorController: UIViewController {
     
     func repositionViews() {
         self.view.subviews.forEach { $0.removeFromSuperview() }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        repositionViews()
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        DispatchQueue.main.async {
+            [weak self] in
+            self?.repositionViews()
+        }
+    }
     }
 }
