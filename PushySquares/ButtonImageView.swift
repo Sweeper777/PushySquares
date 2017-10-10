4d829843efb8fb4c075ea4e6d68425b397755f03
Sweeper777 <sumulang@gmail.com>
2017-10-10 06:43:01 +0100

    added imageRect property

--- PushySquares/ButtonImageView.swift
+++ PushySquares/ButtonImageView.swift
@@ -4,4 +4,23 @@ class ButtonImageView: UIImageView {
     
     var onClick: (() -> ())?
     
+    var imageRect: CGRect {
+        let imageViewSize = self.frame.size
+        let imgSize = self.image?.size
+        
+        guard let imageSize = imgSize, imgSize != nil else {
+            return CGRect.zero
+        }
+        
+        let scaleWidth = imageViewSize.width / imageSize.width
+        let scaleHeight = imageViewSize.height / imageSize.height
+        let aspect = fmin(scaleWidth, scaleHeight)
+        
+        var imageRect = CGRect(x: 0, y: 0, width: imageSize.width * aspect, height: imageSize.height * aspect)
+        // Center image
+        imageRect.origin.x = (imageViewSize.width - imageRect.size.width) / 2
+        imageRect.origin.y = (imageViewSize.height - imageRect.size.height) / 2
+        
+        return imageRect
+    }
 }
