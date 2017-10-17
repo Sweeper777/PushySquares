6782426c2ee39639af2840fde072eb351833a681
Sweeper777 <sumulang@gmail.com>
2017-10-17 07:53:44 +0100

    added notes for the Heuristic Evaluation Function that I'm gonna write

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,6 +1,16 @@
 import PushySquaresModel
 import UIKit
 
+/*
+Heuristic Evaluation Function Algorithm:
+ - life loss, if >= current lives then -inf, -1 each * 1000
+ - opponent life loss, if 2 players left, difference between after self life and after opponent life * 10000, otherwise +1 each * 1000
+ - spread (max of maxX and maxY), -1 each * 100 if squares < 4 else 1
+ - opponent spread, +1 each * 1 if squares < 4, else 10
+ - in danger (be pushed off next), -1 each * 100 if current life > count, else inf
+ - opponent in danger, +1 each * 1 if squares < 4, else 100
+*/
+
 func printBoard(_ board: Array2D<Tile>) {
     for y in 0..<board.columns {
         for x in 0..<board.rows {
@@ -31,32 +41,3 @@ func printBoard(_ board: Array2D<Tile>) {
 }
 
 let game = Game(map: .standard, playerCount: 4)
-game.moveUp()
-game.moveDown()
-game.moveDown()
-game.moveUp()
-game.moveUp()
-game.moveDown()
-game.moveDown()
-game.moveUp()
-game.moveUp()
-game.moveDown()
-game.moveDown()
-game.moveUp()
-game.moveUp()
-game.moveDown()
-game.moveRight()
-game.moveDown()
-game.moveUp()
-game.moveDown()
-game.moveRight()
-game.moveLeft()
-game.moveRight()
-game.moveUp()
-game.moveRight()
-game.moveLeft()
-game.moveRight()
-game.moveDown()
-game.moveDown()
-game.moveUp()
-printBoard(game.board)
