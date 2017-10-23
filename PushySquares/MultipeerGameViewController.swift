79f6276ec4ad7a9aa13bf7045a5923d99da397fb
Sweeper777 <sumulang@gmail.com>
2017-10-23 09:53:23 +0100

    added myColor property

--- PushySquares/MultipeerGameViewController.swift
+++ PushySquares/MultipeerGameViewController.swift
@@ -8,4 +8,17 @@ class MultipeerGameViewController: GameViewController {
     var playerColorsDict: [MCPeerID: Color]!
     var disconnectHandled = false
     
+    var myColor: Color! {
+        didSet {
+            let color = GameBoardView.colorToUIColor[myColor]!
+            UIGraphicsBeginImageContextWithOptions(CGSize(width: 56, height: 56), false, 0)
+            color.setFill()
+            UIRectFill(CGRect.zero.with(width: 56).with(height: 56))
+            let image = UIGraphicsGetImageFromCurrentImageContext()!
+            UIGraphicsEndImageContext()
+            let alert = SCLAlertView(appearance: SCLAlertView.SCLAppearance(kCircleIconHeight: 56, showCloseButton: false))
+            alert.addButton("OK", action: {})
+            _ = alert.showCustom("Your color is \(GameBoardView.colorToString[myColor]!).", subTitle: "", color: .black, icon: image)
+        }
+    }
 }
