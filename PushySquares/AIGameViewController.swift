a205f38a0b2b04e2781c012f95d3a1a4fbc34bab
Sweeper777 <sumulang@gmail.com>
2017-11-11 08:32:34 +0000

    overrode newGame method

--- PushySquares/AIGameViewController.swift
+++ PushySquares/AIGameViewController.swift
@@ -2,4 +2,8 @@ import UIKit
 
 class AIGameViewController: GameViewController {
     var aiCount: Int!
+    
+    override func newGame() -> Game {
+        return Game(map: .standard, playerCount: playerCount + aiCount)
+    }
 }
