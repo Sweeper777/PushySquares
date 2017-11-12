0ab3a6ff3a81080de6e9508a61ef5ad3949b4f4e
Sweeper777 <sumulang@gmail.com>
2017-11-12 07:26:51 +0000

    overrode animationDidComplete

--- PushySquares/AIGameViewController.swift
+++ PushySquares/AIGameViewController.swift
@@ -6,4 +6,17 @@ class AIGameViewController: GameViewController {
     override func newGame() -> Game {
         return Game(map: .standard, playerCount: playerCount + aiCount)
     }
+    
+    override func animationDidComplete() {
+        if game.currentPlayer.color != .color1 {
+            allGR.forEach { $0.isEnabled = false }
+            if game.players.filter({$0.lives > 0}).count > 1 {
+                DispatchQueue.main.async { [weak self] in
+                    guard let `self` = self else { return }
+                    let ai = GameAI(game: self.game.createCopy(), myColor: self.game.currentPlayer.color, wSelfLife: 553, wDiffLives: 8371, wSquareThreshold: 3, wSelfSpreadBelowThreshold: 5646, wSelfSpreadAboveThreshold: 3791, wOpponentSpread: 8583, wSelfInDanger: 6187, wOpponentInDangerBelowThreshold: 680, wOpponentInDangerAboveThreshold: 9157)
+                    self.game.moveInDirection(ai.getNextMove())
+                }
+            }
+        }
+    }
 }
