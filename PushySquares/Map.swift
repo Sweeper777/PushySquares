2857fcb48141e9e2a788d2806fef1134cf033e45
Sweeper777 <sumulang@gmail.com>
2017-11-13 17:40:05 +0000

    looped through the contents of file

--- PushySquares/Map.swift
+++ PushySquares/Map.swift
@@ -37,6 +37,24 @@ public struct Map {
         let lines = fileContents.components(separatedBy: "\n").filter({!$0.isEmpty})
         var board = Array2D<Tile>(columns: lines.first!.characters.count, rows: lines.count, initialValue: .void)
         var spawnpoints = [Color: Position]()
+        for (x, line) in lines.enumerated() {
+            for (y, c) in line.characters.enumerated() {
+                board[x, y] = dict[c] ?? .empty
+                switch c {
+                case "1":
+                    spawnpoints[.color1] = Position(x, y)
+                case "2":
+                    spawnpoints[.color2] = Position(x, y)
+                case "3":
+                    spawnpoints[.color3] = Position(x, y)
+                case "4":
+                    spawnpoints[.color4] = Position(x, y)
+                default: break
+                }
+            }
+        }
+        self.board = board
+        self.spawnpoints = spawnpoints
     }
 }
 
