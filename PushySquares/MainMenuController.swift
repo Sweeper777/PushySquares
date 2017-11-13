9febf06f9dc366c1c4aba5363baf1e51abad89e0
Sweeper777 <sumulang@gmail.com>
2017-11-13 17:41:02 +0000

    tested new initializer

--- PushySquares/MainMenuController.swift
+++ PushySquares/MainMenuController.swift
@@ -105,6 +105,10 @@ class MainMenuController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        let path = Bundle.main.path(forResource: "dummy", ofType: "map")
+        let map = Map(file: path!)
+        printBoard(map.board)
+        print(map.spawnpoints)
         repositionViews()
     }
     
