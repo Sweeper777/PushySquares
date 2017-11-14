8e34c2624372eaad621e4e592b95ec6e39dee5a6
Sweeper777 <sumulang@gmail.com>
2017-11-14 07:25:09 +0000

    added randomness to AI's search depth

--- PushySquares/GameAI.swift
+++ PushySquares/GameAI.swift
@@ -1,4 +1,4 @@
-
+import Foundation
 extension Game {
     func createCopy() -> Game {
         return Game(game: self)
@@ -214,6 +214,6 @@ class GameAI {
     }
     
     func getNextMove() -> Direction {
-        return minimax(depth: 6, color: myColor).direction
+        return minimax(depth: Int(arc4random_uniform(5) + 2), color: myColor).direction
     }
 }
