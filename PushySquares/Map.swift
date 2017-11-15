8cdabb5a72f13c2bc63b7303e9fbdbf946eed283
Sweeper777 <sumulang@gmail.com>
2017-11-15 07:17:36 +0000

    changed declarations of standard map

--- PushySquares/Map.swift
+++ PushySquares/Map.swift
@@ -4,23 +4,7 @@ public struct Map {
     public let board: Array2D<Tile>
     public let spawnpoints: [Color: Position]
     
-    public static let standard = Map(board: [
-        [.void, .void,  .void,  .void,  .wall,  .wall,  .void,  .void,  .void,  .void,],
-        [.void, .empty, .empty, .empty, .empty, .empty, .empty, .empty, .empty, .void],
-        [.void, .empty, .empty, .empty, .empty, .empty, .empty, .empty, .empty, .void],
-        [.void, .empty, .empty, .empty, .empty, .empty, .empty, .empty, .empty, .void],
-        [.wall, .empty, .empty, .empty, .wall,  .wall,  .empty, .empty, .empty, .wall],
-        [.wall, .empty, .empty, .empty, .wall,  .wall,  .empty, .empty, .empty, .wall],
-        [.void, .empty, .empty, .empty, .empty, .empty, .empty, .empty, .empty, .void],
-        [.void, .empty, .empty, .empty, .empty, .empty, .empty, .empty, .empty, .void],
-        [.void, .empty, .empty, .empty, .empty, .empty, .empty, .empty, .empty, .void],
-        [.void, .void,  .void,  .void,  .wall,  .wall,  .void,  .void,  .void,  .void,]
-        ], spawnpoints: [
-            .color1: Position(1, 1),
-            .color2: Position(8, 1),
-            .color3: Position(8, 8),
-            .color4: Position(1, 8)
-        ])
+    public static let standard = Map(file: Bundle.main.path(forResource: "standard", ofType: "map")!)
     
     public init(board: Array2D<Tile>, spawnpoints: [Color: Position]) {
         self.board = board
