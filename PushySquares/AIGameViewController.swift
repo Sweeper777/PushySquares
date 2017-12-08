eea27945397bb15dfb8dd39eba32bea39df89d56
Sweeper777 <sumulang@gmail.com>
2017-12-08 06:50:01 +0000

    showed player color

--- PushySquares/AIGameViewController.swift
+++ PushySquares/AIGameViewController.swift
@@ -38,6 +38,16 @@ class AIGameViewController: GameViewController {
                 self.game.moveInDirection(ai.getNextMove())
             }
         }
+        let myColor = playerColors.first!
+        let color = GameBoardView.colorToUIColor[myColor]!
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: 56, height: 56), false, 0)
+        color.setFill()
+        UIRectFill(CGRect.zero.with(width: 56).with(height: 56))
+        let image = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        let alert = SCLAlertView(appearance: SCLAlertView.SCLAppearance(kCircleIconHeight: 56, showCloseButton: false))
+        alert.addButton("OK", action: {})
+        _ = alert.showCustom("Your color is \(GameBoardView.colorToString[myColor]!).", subTitle: "", color: .black, icon: image)
     }
     
     override func restartTapped() {
@@ -51,6 +61,16 @@ class AIGameViewController: GameViewController {
             self.statusBar.setNewSquareIn(value: self.game.currentPlayer.turnsUntilNewSquare)
             self.statusBar.setCurrentTurn(value: self.game.currentPlayer.color)
             self.statusBar.setLives(players: self.game.players)
+            let myColor = self.playerColors.first!
+            let color = GameBoardView.colorToUIColor[myColor]!
+            UIGraphicsBeginImageContextWithOptions(CGSize(width: 56, height: 56), false, 0)
+            color.setFill()
+            UIRectFill(CGRect.zero.with(width: 56).with(height: 56))
+            let image = UIGraphicsGetImageFromCurrentImageContext()!
+            UIGraphicsEndImageContext()
+            let alert = SCLAlertView(appearance: SCLAlertView.SCLAppearance(kCircleIconHeight: 56, showCloseButton: false))
+            alert.addButton("OK", action: {})
+            _ = alert.showCustom("Your color is \(GameBoardView.colorToString[myColor]!).", subTitle: "", color: .black, icon: image)
             if self.playerColors.contains(self.game.currentPlayer.color) {
                 self.allGR.forEach { $0.isEnabled = true }
             } else {
