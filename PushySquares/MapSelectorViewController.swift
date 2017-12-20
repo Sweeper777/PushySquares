4cccb3f0c31a4d23f25610b25724face32dc81da
Sweeper777 <sumulang@gmail.com>
2017-12-20 09:19:21 +0800

    added selectedMap property

--- PushySquares/MapSelectorViewController.swift
+++ PushySquares/MapSelectorViewController.swift
@@ -3,6 +3,11 @@ import FSPagerView
 import SwiftyButton
 
 class MapSelectorViewController: UIViewController {
+    var selectedMap: String? {
+        didSet {
+            delegate?.didSelectMap(mapSelectorController: self)
+        }
+    }
     weak var delegate: MapSelectorViewControllerDelegate?
     
     var pageView: FSPagerView!
