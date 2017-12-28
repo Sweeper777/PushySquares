91757dd6644f8395652bc235172aa2f1db60ebee
Sweeper777 <sumulang@gmail.com>
2017-12-28 08:27:34 +0800

    Handled the case of no human players

--- PushySquares/AIGameViewController.swift
+++ PushySquares/AIGameViewController.swift
@@ -38,16 +38,17 @@ class AIGameViewController: GameViewController {
                 self.game.moveInDirection(ai.getNextMove())
             }
         }
-        let myColor = playerColors.first!
-        let color = GameBoardView.colorToUIColor[myColor]!
-        UIGraphicsBeginImageContextWithOptions(CGSize(width: 56, height: 56), false, 0)
-        color.setFill()
-        UIRectFill(CGRect.zero.with(width: 56).with(height: 56))
-        let image = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
-        let alert = SCLAlertView(appearance: SCLAlertView.SCLAppearance(kCircleIconHeight: 56, showCloseButton: false))
-        alert.addButton("OK", action: {})
-        _ = alert.showCustom("Your color is \(GameBoardView.colorToString[myColor]!).", subTitle: "", color: .black, icon: image)
+        if let myColor = playerColors.first {
+            let color = GameBoardView.colorToUIColor[myColor]!
+            UIGraphicsBeginImageContextWithOptions(CGSize(width: 56, height: 56), false, 0)
+            color.setFill()
+            UIRectFill(CGRect.zero.with(width: 56).with(height: 56))
+            let image = UIGraphicsGetImageFromCurrentImageContext()!
+            UIGraphicsEndImageContext()
+            let alert = SCLAlertView(appearance: SCLAlertView.SCLAppearance(kCircleIconHeight: 56, showCloseButton: false))
+            alert.addButton("OK", action: {})
+            _ = alert.showCustom("Your color is \(GameBoardView.colorToString[myColor]!).", subTitle: "", color: .black, icon: image)
+        }
     }
     
     override func restartTapped() {
