2fa0f4e4466a4c7fac501c8b4954912ca943a5d2
Sweeper777 <sumulang@gmail.com>
2018-01-10 14:03:31 +0000

    added locked property in GameBoardCell

--- PushySquares/GameBoardCell.swift
+++ PushySquares/GameBoardCell.swift
@@ -1,10 +1,18 @@
 import FSPagerView
+import UIKit
 
 class GameBoardCell: FSPagerViewCell {
     @IBOutlet var gameBoardView: GameBoardView!
+    @IBOutlet var lockedImage: UIImageView!
     var game: Game! {
         didSet {
             gameBoardView.game = game
+        }
+    }
+    
+    var locked: Bool = true {
+        didSet {
+            lockedImage.isHidden = !locked
         }
     }
 }
