9d482f275edbefbe8d9fc1bcd83c4c7b82bf40b6
Sweeper777 <sumulang@gmail.com>
2018-01-11 16:01:14 +0000

    added prompt to unlock maps

--- PushySquares/MapSelectorViewController.swift
+++ PushySquares/MapSelectorViewController.swift
@@ -128,5 +128,11 @@ extension MapSelectorViewController: FSPagerViewDelegate, FSPagerViewDataSource 
     
     func pagerView(_ pagerView: FSPagerView, shouldSelectItemAt index: Int) -> Bool {
         return false
+    fileprivate func promptUnlockMaps() {
+        let alert = SCLAlertView(appearance: SCLAlertView.SCLAppearance(showCloseButton: false))
+        alert.addButton("Unlock All Maps") { [weak self] in
+        alert.addButton("Cancel", action: {})
+        alert.showInfo("This map is locked!", subTitle: "Do you want to unlock all locked maps?", circleIconImage: #imageLiteral(resourceName: "lockedIcon"))
+    }
     }
 }
