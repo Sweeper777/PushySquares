a2fdfab59e812545141b65747c97de372d893688
Sweeper777 <sumulang@gmail.com>
2018-01-12 08:01:14 +0000

    added requestDidFail with error

--- PushySquares/MapSelectorViewController.swift
+++ PushySquares/MapSelectorViewController.swift
@@ -173,6 +173,12 @@ extension MapSelectorViewController: SKProductsRequestDelegate {
             showIAPError(message: "Unable to get product information. Please check your Internet connection.")
         }
     }
+    
+    func request(_ request: SKRequest, didFailWithError error: Error) {
+        EZLoadingActivity.hide()
+        showIAPError(message: "Unable to get product information. Please check your Internet connection.")
+    }
+    
     func showIAPError(message: String) {
         let alert = SCLAlertView(appearance: SCLAlertView.SCLAppearance(showCloseButton: false))
         alert.addButton("OK", action: {})
