1a260992cf4c5a4cba9da5d427b2e6b02ad80c14
Sweeper777 <sumulang@gmail.com>
2018-01-23 16:41:23 +0000

    added displacementFunction property to Direction

--- PushySquares/Tile.swift
+++ PushySquares/Tile.swift
@@ -18,4 +18,17 @@ public enum Direction {
     case down
     case left
     case right
+    
+    public var displacementFunction: (Position) -> Position {
+        switch self {
+        case .up:
+            return { $0.above() }
+        case .down:
+            return { $0.below() }
+        case .left:
+            return { $0.left() }
+        case .right:
+            return { $0.right() }
+        }
+    }
 }
