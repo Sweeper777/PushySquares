1638bafa0c904c4384fb1d3d3f11ab73bc9519e6
Sweeper777 <sumulang@gmail.com>
2018-01-25 18:38:54 +0000

    added slipDown animation

--- PushySquares/SquareView.swift
+++ PushySquares/SquareView.swift
@@ -68,6 +68,16 @@ class SquareView: UIView {
         }
     }
     
+    var slipDown: Animate {
+        return Animate(duration: SquareView.animDuration, delay: 0, options: [.curveEaseInOut]) {
+            [weak self] in
+            guard let `self` = self else { return }
+            let superView = self.superview as! GameBoardView
+            self.tag = superView.position(forViewTag: self.tag).below().below().hashValue
+            self.frame = self.frame.with(origin: superView.squareViewPoint(for: superView.position(forViewTag: self.tag)))
+        }
+    }
+    
     var destroyed: Animate {
         return transform(duration: SquareView.animDuration, transforms: [
             .scale(x: 0.001, y: 0.001)
