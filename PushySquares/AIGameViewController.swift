0493dc339bd167ef606c66629a4447640eed21b2
Sweeper777 <sumulang@gmail.com>
2019-10-26 09:07:46 +0100

    extracted restart method

--- PushySquares/AIGameViewController.swift
+++ PushySquares/AIGameViewController.swift
@@ -53,36 +53,45 @@ class AIGameViewController: GameViewController {
         alert.addButton("Yes".localized, action: {
             [weak self] in
             guard let `self` = self else { return }
-            self.game = self.newGame()
-            self.game.delegate = self
-            self.boardView.game = self.game
-            self.statusBar.setNewSquareIn(value: self.game.currentPlayer.turnsUntilNewSquare)
-            self.statusBar.setCurrentTurn(value: self.game.currentPlayer.color)
-            self.statusBar.setLives(players: self.game.players)
-            if let myColor = self.playerColors.first {
-                let color = GameBoardView.colorToUIColor[myColor]!
-                UIGraphicsBeginImageContextWithOptions(CGSize(width: 56, height: 56), false, 0)
-                color.setFill()
-                UIRectFill(CGRect.zero.with(width: 56).with(height: 56))
-                let image = UIGraphicsGetImageFromCurrentImageContext()!
-                UIGraphicsEndImageContext()
-                let alert = SCLAlertView(appearance: SCLAlertView.SCLAppearance(kCircleIconHeight: 56, showCloseButton: false))
-                alert.addButton("OK".localized, action: {})
-                _ = alert.showCustom(String(format: "Your color is %@.".localized, GameBoardView.colorToString[myColor]!), subTitle: "", color: .black, icon: image)
-            }
             if self.playerColors.contains(self.game.currentPlayer.color) {
-                self.allGR.forEach { $0.isEnabled = true }
+                self.restart()
             } else {
-                self.allGR.forEach { $0.isEnabled = false }
-                DispatchQueue.main.async {
-                    self.currentAI = self.aiCount < 2 ? self.twoPlayerAI() : self.multiplayerAI()
-                    self.currentAI.getNextMove(on: self.aiQueue)
-                }
             }
-            self.showHideActionBar()
         })
         alert.addButton("No".localized, action: {})
         alert.showWarning("Confirm".localized, subTitle: "Do you really want to restart?".localized)
+    }
+    
+    func restart() {
+        self.game.delegate = nil
+        self.currentAI = nil
+        self.game = self.newGame()
+        self.game.delegate = self
+        self.boardView.game = self.game
+        self.statusBar.setNewSquareIn(value: self.game.currentPlayer.turnsUntilNewSquare)
+        self.statusBar.setCurrentTurn(value: self.game.currentPlayer.color)
+        self.statusBar.setLives(players: self.game.players)
+        if let myColor = self.playerColors.first {
+            let color = GameBoardView.colorToUIColor[myColor]!
+            UIGraphicsBeginImageContextWithOptions(CGSize(width: 56, height: 56), false, 0)
+            color.setFill()
+            UIRectFill(CGRect.zero.with(width: 56).with(height: 56))
+            let image = UIGraphicsGetImageFromCurrentImageContext()!
+            UIGraphicsEndImageContext()
+            let alert = SCLAlertView(appearance: SCLAlertView.SCLAppearance(kCircleIconHeight: 56, showCloseButton: false))
+            alert.addButton("OK".localized, action: {})
+            _ = alert.showCustom(String(format: "Your color is %@.".localized, GameBoardView.colorToString[myColor]!), subTitle: "", color: .black, icon: image)
+        }
+        if self.playerColors.contains(self.game.currentPlayer.color) {
+            self.allGR.forEach { $0.isEnabled = true }
+        } else {
+            self.allGR.forEach { $0.isEnabled = false }
+            DispatchQueue.main.async {
+                self.currentAI = self.aiCount < 2 ? self.twoPlayerAI() : self.multiplayerAI()
+                self.currentAI.getNextMove(on: self.aiQueue)
+            }
+        }
+        self.showHideActionBar()
     }
     
     fileprivate func twoPlayerAI() -> GameAI {
