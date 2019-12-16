7e7840d24820d39ecc297384b71154ee2efae0de
Mulang Su <sumulang@gmail.com>
2019-12-16 04:51:03 +0200

    added new maps to allMaps array

--- PushySquares/Map.swift
+++ PushySquares/Map.swift
@@ -78,5 +78,24 @@ public func printBoard(_ board: Array2D<Tile>) {
     }
 }
 
-let allMaps = ["standard", "small", "large", "hole", "walls", "zigzag", "quick", "grey1", "grey2", "grey3", "diagonal", "slippery", "superslippery", "morewalls"]
+let allMaps = ["standard",
+               "small",
+               "large",
+               "hole",
+               "walls",
+               "zigzag",
+               "quick",
+               "grey1",
+               "grey2",
+               "grey3",
+               "diagonal",
+               "slippery",
+               "superslippery",
+               "morewalls",
+               "cublex1",
+               "cublex2",
+               "cublex3",
+               "cublex4",
+               "cublex5",
+]
 
