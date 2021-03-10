ba66592771732c77c2c38ed3f4cf6f0da3dbf0e5
Mulang Su <sumulang@gmail.com>
2021-03-10 22:01:09 +0800

    added BoardState.isSquare

--- PushySquaresModel/BoardState.swift
+++ PushySquaresModel/BoardState.swift
@@ -2,4 +2,11 @@ public enum BoardState : Equatable {
     case empty
     case square(Color)
     case deadBody
+    
+    public var isSquare: Bool {
+        if case .square = self {
+            return true
+        }
+        return false
+    }
 }
