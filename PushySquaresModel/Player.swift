c4582a2458b58147915d667a63c60f84eda6e677
Mulang Su <sumulang@gmail.com>
2021-03-11 22:04:24 +0800

    Create Player.swift

---
+++ PushySquaresModel/Player.swift
@@ -0,0 +1,21 @@
+public class Player {
+    public var turnsUntilNewSquare: Int
+    public var lives: Int {
+        didSet {
+            if lives < 0 {
+                lives = 0
+            }
+        }
+    }
+    public let color: Color
+    
+    init(turnsUntilNewSquare: Int, lives: Int, color: Color) {
+        self.turnsUntilNewSquare = turnsUntilNewSquare
+        self.lives = lives
+        self.color = color
+    }
+    
+    func createCopy() -> Player {
+        return Player(turnsUntilNewSquare: self.turnsUntilNewSquare, lives: self.lives, color: self.color)
+    }
+}
