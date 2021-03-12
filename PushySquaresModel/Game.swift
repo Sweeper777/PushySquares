f786c0e65a570c1d9e12b268a6ea0905824824c5
Mulang Su <sumulang@gmail.com>
2021-03-12 21:24:58 +0800

    added Game.init(map:playerCount:lives:)

--- PushySquaresModel/Game.swift
+++ PushySquaresModel/Game.swift
@@ -15,4 +15,40 @@ public class Game {
     
     private var currentPlayerIndex = 0
     
+    public init(map: Map, playerCount: Int, lives: Int = 5) {
+        self.map = map.board
+        self.boardState = map.initialBoardState
+        self.spawnpoints = map.spawnpoints
+        
+        switch playerCount {
+        case 4:
+            players.append(Player(turnsUntilNewSquare: Game.playerCountToTurnsUntilNewSquare[playerCount]!, lives: lives, color: .yellow))
+            spawnNewSquare(color: .yellow)
+            fallthrough
+        case 3:
+            players.append(Player(turnsUntilNewSquare: Game.playerCountToTurnsUntilNewSquare[playerCount]!, lives: lives, color: .blue))
+            spawnNewSquare(color: .blue)
+            fallthrough
+        case 2:
+            players.append(Player(turnsUntilNewSquare: Game.playerCountToTurnsUntilNewSquare[playerCount]!, lives: lives, color: .red))
+            players.append(Player(turnsUntilNewSquare: Game.playerCountToTurnsUntilNewSquare[playerCount]!, lives: lives, color: .green))
+            spawnNewSquare(color: .red)
+            spawnNewSquare(color: .green)
+        default:
+            fatalError()
+        }
+        
+        players.sort { $0.color.rawValue < $1.color.rawValue }
+        
+        if playerCount < 4 {
+            spawnpoints[.yellow] = nil
+        }
+        
+        if playerCount < 3 {
+            spawnpoints[.blue] = nil
+        }
+        
+        currentPlayer.turnsUntilNewSquare -= 1
+    }
+    
 }
