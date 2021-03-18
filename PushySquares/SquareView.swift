61c7d17e61152857a48f5338de333f0b44716409
Mulang Su <sumulang@gmail.com>
2021-03-18 22:08:06 +0800

    implemented SquareView.commonInit

--- PushySquares/SquareView.swift
+++ PushySquares/SquareView.swift
@@ -14,5 +14,16 @@ class SquareView : UIView{
     }
 
     func commonInit() {
+        let strokeWidth = self.width / 8
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: self.width - strokeWidth / 2, y: 0))
+        path.addLine(to: CGPoint(x: self.width - strokeWidth / 2, y: self.height - strokeWidth / 2))
+        path.addLine(to: CGPoint(x: 0, y: self.height - strokeWidth / 2))
+        let shadowLayer = CAShapeLayer()
+        shadowLayer.strokeColor = UIColor(white: 0, alpha: 0.3).cgColor
+        shadowLayer.fillColor = UIColor.clear.cgColor
+        shadowLayer.path = path.cgPath
+        shadowLayer.lineWidth = strokeWidth
+        layer.addSublayer(shadowLayer)
     }
 }
