bee9ad5d90ddc34c3e649a3e0433126b9a498c03
Mulang Su <sumulang@gmail.com>
2021-03-21 10:09:31 +0800

    added a bunch of helpful extensions

---
+++ PushySquares/Helpers.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+fileprivate func shouldContinueToEnlarge(targetSize: CGSize, currentSize: CGSize) -> Bool {
+    return targetSize.height > currentSize.height && targetSize.width > currentSize.width
+}
+
+func fontSizeThatFits(size: CGSize, text: NSString, font: UIFont) -> CGFloat {
+    var fontToTest = font.withSize(0)
+    var currentSize = text.size(withAttributes: [NSAttributedString.Key.font: fontToTest])
+    var fontSize = CGFloat(1)
+    while shouldContinueToEnlarge(targetSize: size, currentSize: currentSize) {
+        fontToTest = fontToTest.withSize(fontSize)
+        currentSize = text.size(withAttributes: [NSAttributedString.Key.font: fontToTest])
+        fontSize += 1
+    }
+    return fontSize - 1
+}
+
+extension String {
+    var localized: String {
+        NSLocalizedString(self, comment: "")
+    }
+}
