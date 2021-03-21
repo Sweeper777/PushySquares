7b06ae0c4f0215071ee1f8e4b628684cb36969af
Mulang Su <sumulang@gmail.com>
2021-03-21 10:08:28 +0800

    made square view automatically layout its shadow

--- PushySquares/SquareView.swift
+++ PushySquares/SquareView.swift
@@ -3,6 +3,8 @@ import SwiftyUtils
 
 class SquareView : UIView{
 
+    var shadowLayer: CAShapeLayer!
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         commonInit()
@@ -14,16 +16,28 @@ class SquareView : UIView{
     }
 
     func commonInit() {
+        addShadowLayer()
+    }
+
+    func addShadowLayer() {
         let strokeWidth = self.width / 8
         let path = UIBezierPath()
         path.move(to: CGPoint(x: self.width - strokeWidth / 2, y: 0))
         path.addLine(to: CGPoint(x: self.width - strokeWidth / 2, y: self.height - strokeWidth / 2))
         path.addLine(to: CGPoint(x: 0, y: self.height - strokeWidth / 2))
-        let shadowLayer = CAShapeLayer()
+        shadowLayer = CAShapeLayer()
         shadowLayer.strokeColor = UIColor(white: 0, alpha: 0.3).cgColor
         shadowLayer.fillColor = UIColor.clear.cgColor
         shadowLayer.path = path.cgPath
         shadowLayer.lineWidth = strokeWidth
         layer.addSublayer(shadowLayer)
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        shadowLayer.removeFromSuperlayer()
+        addShadowLayer()
+        var a = [1]
+        a.remove(atOffsets: IndexSet())
     }
 }
