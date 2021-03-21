21cb69db6ed87c8068f5be8871b5fb8fe5f0cbea
Mulang Su <sumulang@gmail.com>
2021-03-21 10:10:11 +0800

    displayed the initial game state in status bar

--- PushySquares/ViewController.swift
+++ PushySquares/ViewController.swift
@@ -4,10 +4,16 @@ import PushySquaresModel
 class ViewController: UIViewController {
 
     @IBOutlet var board: BoardView!
+    @IBOutlet var statusBar: StatusBar!
+
+    let game = Game(map: .standard, playerCount: 4)
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        board.board = Game(map: .standard, playerCount: 4)
-
+        board.board = game
+        statusBar.setCurrentTurn(game.currentPlayer.color)
+        statusBar.setLives(players: game.players)
+        statusBar.setNewSquareIn(game.currentPlayer.turnsUntilNewSquare)
     }
 }
 
