1ecb4bcdb30cd9b7f5ef404f2ec9fc12b73b4743
Mulang Su <sumulang@gmail.com>
2021-03-22 15:25:38 +0800

    added AnimationManager.runAnimation

--- PushySquares/AnimationManager.swift
+++ PushySquares/AnimationManager.swift
@@ -13,4 +13,21 @@ class AnimationManager<Phase: AnimationPhase> {
         phases.append(Phase(duration: duration))
     }
 
+    func runAnimation(completion: (() -> Void)?) {
+        let indices = phases.indices
+        for (i, next) in zip(indices, indices.dropFirst()) {
+            phases[i].onEnd = { [weak self] in
+                guard let `self` = self else { return }
+                self.completionHandlers[i]?()
+                self.phases[next].start(animations: self.groups[next])
+            }
+        }
+        if let first = phases.first, let last = phases.last {
+            last.onEnd = completion
+            first.start(animations: groups.first!)
+        } else {
+            completion?()
+        }
+    }
+
 }
