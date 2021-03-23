5b3486cfdb68d47a1f6cf1315a2278eaea2950c0
Mulang Su <sumulang@gmail.com>
2021-03-23 22:05:11 +0800

    added animationBlock(for:)

--- PushySquares/ViewAnimationPhase.swift
+++ PushySquares/ViewAnimationPhase.swift
@@ -11,6 +11,18 @@ class ViewAnimationPhase: AnimationPhase {
     func start(animations: [AnimationType: [UIView]]) {
     }
 
+    private func animationBlock(for type: AnimationType) -> (UIView) -> Void {
+        switch type {
+        case .move(let dx, let dy):
+            return { $0.frame = $0.frame.applying(CGAffineTransform(translationX: dx.f, y: dy.f)) }
+        case .fall:
+            return { $0.transform = CGAffineTransform(scaleX: ViewAnimationPhase.invisibleScale, y: ViewAnimationPhase.invisibleScale) }
+        case .grayOut:
+            return { $0.backgroundColor = .gray }
+        case .newSquare:
+            return { $0.transform = CGAffineTransform(scaleX: 1, y: 1) }
+        }
+    }
 
     required init(duration: TimeInterval) {
         self.duration = duration
