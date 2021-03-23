620538ca650704799f2c096fbe07259dd1a4b2ab
Mulang Su <sumulang@gmail.com>
2021-03-23 22:10:36 +0800

    implemented ViewAnimationPhase.start

--- PushySquares/ViewAnimationPhase.swift
+++ PushySquares/ViewAnimationPhase.swift
@@ -9,6 +9,13 @@ class ViewAnimationPhase: AnimationPhase {
     var onEnd: (() -> Void)?
 
     func start(animations: [AnimationType: [UIView]]) {
+        UIView.animate(withDuration: duration, animations: { [weak self] in
+            guard let `self` = self else { return }
+            for (type, views) in animations {
+                let block = self.animationBlock(for: type)
+                views.forEach(block)
+            }
+        }, completion: { [weak self] _ in self?.onEnd?() })
     }
 
     private func animationBlock(for type: AnimationType) -> (UIView) -> Void {
