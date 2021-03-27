28271f633a96ae93ebd2cff3fa87cec6d8b944a2
Mulang Su <sumulang@gmail.com>
2021-03-27 11:10:05 +0800

    added some properties to AIGameVC

--- PushySquares/AIGameViewController.swift
+++ PushySquares/AIGameViewController.swift
@@ -1,6 +1,11 @@
 import PushySquaresModel
 import UIKit
+import SCLAlertView
 
 class AIGameViewController : GameViewController {
-    
+    var hasHumanPlayer = true
+    var humanPlayerColor: Color?
+    let aiQueue = DispatchQueue(label: "gameAI", qos: .background)
+    var currentAI: GameAI?
+
 }
