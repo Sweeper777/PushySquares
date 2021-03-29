2c5eb7565531af921651e8fc30406c9bdffa9f37
Mulang Su <sumulang@gmail.com>
2021-03-29 16:03:37 +0800

    overridden restartGame

--- PushySquares/AIGameViewController.swift
+++ PushySquares/AIGameViewController.swift
@@ -8,6 +8,21 @@ class AIGameViewController : GameViewController {
     let aiQueue = DispatchQueue(label: "gameAI", qos: .userInitiated)
     var currentAI: GameAI?
 
+    override func restartGame() {
+        super.restartGame()
+        if hasHumanPlayer {
+            humanPlayerColor = Color.allCases.randomElement()
+            let alert = SCLAlertView(appearance: SCLAlertView.SCLAppearance(kCircleIconHeight: 56, showCloseButton: false))
+            alert.addButton("OK", action: {})
+            _ = alert.showCustom(
+                    String(format: "Your color is %@.".localized, BoardView.colorToString[humanPlayerColor!]!),
+                    subTitle: "", color: .black,
+                    icon: BoardView.colorToUIColor[humanPlayerColor!]!.image(size: CGSize(width: 56, height: 56)))
+        } else {
+            humanPlayerColor = nil
+        }
+        tryAIMove()
+    }
 
     var isAITurn: Bool {
         game.currentPlayer.color != humanPlayerColor
