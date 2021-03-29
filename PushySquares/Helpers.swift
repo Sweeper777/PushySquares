e3db7aead5ca3140d592f142e06602bc4160c1ca
Mulang Su <sumulang@gmail.com>
2021-03-29 16:03:21 +0800

    added UIColor.desaturated extension

--- PushySquares/Helpers.swift
+++ PushySquares/Helpers.swift
@@ -32,4 +32,13 @@ extension UIColor {
         UIGraphicsEndImageContext()
         return image
     }
+
+    func desaturated() -> UIColor {
+        var hue: CGFloat = 0
+        var saturation: CGFloat = 0
+        var brightness: CGFloat = 0
+        var alpha: CGFloat = 0
+        getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha)
+        return UIColor(hue: hue, saturation: max(0, saturation - 0.2), brightness: brightness, alpha: alpha)
+    }
 }
