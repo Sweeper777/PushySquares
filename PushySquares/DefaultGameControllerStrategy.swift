17248589d42ea8d9bd5a3d7cf53743a1a0aa0186
Mulang Su <sumulang@gmail.com>
2021-03-31 09:59:43 +0800

    Create DefaultGameControllerStrategy.swift

---
+++ PushySquares/DefaultGameControllerStrategy.swift
@@ -0,0 +1,21 @@
+import UIKit
+import PushySquaresModel
+
+class DefaultGameControllerStrategy : GameControllerStrategy {
+    func didRestartGame() {
+
+    }
+
+    func didEndAnimatingMoveResult(_ moveResult: MoveResult) {
+
+    }
+
+    func makeMenuButtons() -> [UIView]? {
+        nil
+    }
+
+    func willMove(_ direction: Direction) {
+
+    }
+
+}
