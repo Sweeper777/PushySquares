a555b06a0fd1e598a6c3d6388103c771ca00c529
Mulang Su <sumulang@gmail.com>
2021-04-01 09:44:57 +0800

    set up the back and start buttons

--- PushySquares/GameModeSelectorViewController.swift
+++ PushySquares/GameModeSelectorViewController.swift
@@ -3,4 +3,35 @@ import FSPagerView
 import SwiftyButton
 
 class GameModeSelectorViewController: UIViewController {
+    @IBOutlet var backButton: PressableButton!
+    @IBOutlet var startButton: PressableButton!
+    @IBOutlet var playerCountSelector: FSPagerView!
+    @IBOutlet var mapSelector: FSPagerView!
+    @IBOutlet var playerCountSelectorPageControl: FSPageControl!
+    @IBOutlet var mapSelectorPageControl: FSPageControl!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        startButton.setTitle("START".localized, for: .normal)
+        startButton.colors = PressableButton.ColorSet(
+                button: UIColor.green.desaturated().darker(),
+                shadow: UIColor.green.desaturated().darker().darker())
+        startButton.addTarget(self, action: #selector(startTapped), for: .touchUpInside)
+
+        backButton.setTitle("BACK".localized, for: .normal)
+        backButton.colors = PressableButton.ColorSet(
+                button: UIColor.green.desaturated().darker(),
+                shadow: UIColor.green.desaturated().darker().darker())
+        backButton.addTarget(self, action: #selector(backTapped), for: .touchUpInside)
+    }
+
+    @objc func startTapped() {
+
+    }
+
+
+    @objc func backTapped() {
+
+    }
 }
