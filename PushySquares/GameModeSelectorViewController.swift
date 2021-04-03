f80d2d8d8c250ce4b1680f7a2bdac6ca1aabe999
Mulang Su <sumulang@gmail.com>
2021-04-03 09:29:06 +0800

    configured the page controls and page views

--- PushySquares/GameModeSelectorViewController.swift
+++ PushySquares/GameModeSelectorViewController.swift
@@ -1,6 +1,7 @@
 import UIKit
 import FSPagerView
 import SwiftyButton
+import PushySquaresModel
 
 class GameModeSelectorViewController: UIViewController {
     @IBOutlet var backButton: PressableButton!
@@ -9,6 +10,13 @@ class GameModeSelectorViewController: UIViewController {
     @IBOutlet var mapSelector: FSPagerView!
     @IBOutlet var playerCountSelectorPageControl: FSPageControl!
     @IBOutlet var mapSelectorPageControl: FSPageControl!
+
+    let gameModes = ["2player", "3player", "4player", "playervsai", "playervs3ai", "4ai"]
+    let maps = allMaps.map {
+        name -> Map in
+        let url = Bundle.main.url(forResource: name, withExtension: "map")!
+        return Map(file: url)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +32,24 @@ class GameModeSelectorViewController: UIViewController {
                 button: UIColor.gray,
                 shadow: UIColor.gray.darker())
         backButton.addTarget(self, action: #selector(backTapped), for: .touchUpInside)
+
+        playerCountSelector.register(FSPagerViewCell.self, forCellWithReuseIdentifier: "cell")
+        playerCountSelector.transformer = FSPagerViewTransformer(type: .linear)
+        playerCountSelectorPageControl.numberOfPages = gameModes.count
+        playerCountSelectorPageControl.currentPage = playerCountSelector.currentIndex
+        playerCountSelectorPageControl.setStrokeColor(.black, for: .normal)
+        playerCountSelectorPageControl.setStrokeColor(.black, for: .selected)
+        playerCountSelectorPageControl.setFillColor(.clear, for: .normal)
+        playerCountSelectorPageControl.setFillColor(.black, for: .selected)
+
+        mapSelector.register(UINib(nibName: "GameBoardCell", bundle: nil), forCellWithReuseIdentifier: "cell")
+        mapSelector.transformer = FSPagerViewTransformer(type: .linear)
+        mapSelectorPageControl.numberOfPages = maps.count
+        mapSelectorPageControl.currentPage = mapSelector.currentIndex
+        mapSelectorPageControl.setStrokeColor(.black, for: .normal)
+        mapSelectorPageControl.setStrokeColor(.black, for: .selected)
+        mapSelectorPageControl.setFillColor(.clear, for: .normal)
+        mapSelectorPageControl.setFillColor(.black, for: .selected)
     }
 
     @objc func startTapped() {
