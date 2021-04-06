1ae8f3e004378229ebb29b7da47865f10d5a6ff6
Mulang Su <sumulang@gmail.com>
2021-04-06 09:21:59 +0800

    set up the views in host VC

--- PushySquares/HostViewController.swift
+++ PushySquares/HostViewController.swift
@@ -28,4 +28,53 @@ class HostViewController: UIViewController, HasMapSelector {
     lazy var browser = MCNearbyServiceBrowser(peer: peerID, serviceType: "pushysquares\(Bundle.main.appBuild)")
     lazy var session = MCSession(peer: peerID)
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        startButton.setTitle("START".localized, for: .normal)
+        startButton.colors = PressableButton.ColorSet(
+                button: UIColor.green.desaturated().darker(),
+                shadow: UIColor.green.desaturated().darker().darker())
+        startButton.addTarget(self, action: #selector(startTapped), for: .touchUpInside)
+
+        backButton.setTitle("BACK".localized, for: .normal)
+        backButton.colors = PressableButton.ColorSet(
+                button: UIColor.gray,
+                shadow: UIColor.gray.darker())
+        backButton.addTarget(self, action: #selector(backTapped), for: .touchUpInside)
+
+        hintLabel.text = "Nearby devices that are willing to join a game is shown below. Tap on a device to connect it to the game.".localized
+
+        setupMapSelector()
+
+        foundPeers.asObservable().bind(to: tableView.rx.items(cellIdentifier: "cell")) {
+            row, model, cell in
+            cell.backgroundColor = .clear
+            cell.textLabel!.text = model.peerID.displayName
+//            cell.textLabel!.font = UIFont(name: "Chalkboard SE", size: cell.textLabel!.font.pointSize)
+//            cell.detailTextLabel!.font = UIFont(name: "Chalkboard SE", size: cell.detailTextLabel!.font.pointSize)
+            switch model.state {
+            case .connected:
+                cell.detailTextLabel!.text = "Connected".localized
+            case .connecting:
+                cell.detailTextLabel!.text = "Connecting...".localized
+            case .error:
+                cell.detailTextLabel!.text = "Unable to connect".localized
+            case .notConnected:
+                cell.detailTextLabel!.text = ""
+            }
+        }.disposed(by: disposeBag)
+
+        browser.delegate = self
+        session.delegate = self
+    }
+
+    @objc func startTapped() {
+
+    }
+
+    @objc func backTapped() {
+
+    }
+
 }
