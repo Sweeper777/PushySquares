59ac1167a6f7d63408a099dba37de7c8ca24385f
Mulang Su <sumulang@gmail.com>
2021-04-13 18:14:34 +0800

    implemented didRestartGame

--- PushySquares/MultipeerGameControllerStrategy.swift
+++ PushySquares/MultipeerGameControllerStrategy.swift
@@ -18,6 +18,22 @@ class MultipeerGameControllerStrategy: NSObject, GameControllerStrategy {
         session.delegate = self
     }
 
+    var myColor: Color {
+        turns[session.myPeerID]!
+    }
+
+    func didRestartGame() {
+        let alert = SCLAlertView(appearance: SCLAlertView.SCLAppearance(kCircleIconHeight: 56, showCloseButton: false))
+        alert.addButton("OK".localized, action: {})
+        _ = alert.showCustom(
+                String(format: "Your color is %@.".localized, BoardView.colorToString[myColor]!),
+                subTitle: "", color: .black,
+                icon: BoardView.colorToUIColor[myColor]!.image(size: CGSize(width: 56, height: 56)))
+        gameViewController.setAllGestureRecognisersEnabled(
+                myColor == gameViewController.game.currentPlayer.color
+        )
+    }
+
 }
 
 extension MultipeerGameControllerStrategy: MCSessionDelegate {
