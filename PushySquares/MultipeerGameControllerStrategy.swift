b1acaf685568a0e111cf816fc45cc5ef40c38cf2
Mulang Su <sumulang@gmail.com>
2021-04-14 10:21:53 +0800

    made the move when receive data

--- PushySquares/MultipeerGameControllerStrategy.swift
+++ PushySquares/MultipeerGameControllerStrategy.swift
@@ -135,6 +135,38 @@ extension MultipeerGameControllerStrategy: MCSessionDelegate {
         _ = alert.showWarning("Oops!".localized, subTitle: String(format: "%@ disconnected from the game.".localized, peerID.displayName))
     }
 
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        guard data.count > 0 else { return }
+
+        let moveResult: MoveResult
+
+        switch DataCodes(rawValue: data[0]) {
+        case .moveLeft?:
+            moveResult = gameViewController.game.moveLeft()
+        case .moveDown?:
+            moveResult = gameViewController.game.moveDown()
+        case .moveUp?:
+            moveResult = gameViewController.game.moveUp()
+        case .moveRight:
+            moveResult = gameViewController.game.moveRight()
+        default:
+            return
+        }
+
+        gameViewController.board.animateMoveResult(moveResult)
+    }
+
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+
+    }
+
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+
+    }
+
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+
+    }
 
 
 }
