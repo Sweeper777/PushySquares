4791ff1bac6cffd6a19f7935f80bb292fecec988
Mulang Su <sumulang@gmail.com>
2021-04-21 09:13:49 +0800

    replaced the image literal

--- PushySquares/HasMapSelector.swift
+++ PushySquares/HasMapSelector.swift
@@ -96,7 +96,7 @@ class InAppPurchaseManager : NSObject, SKProductsRequestDelegate, SKPaymentTrans
                     EZLoadingActivity.show("Loading...".localized, disableUI: true)
                 }
                 alert.addButton("Cancel".localized, action: {})
-                alert.showInfo("Unlock All Maps".localized, subTitle: String(format: "Do you want to unlock all locked maps for %@?".localized, price!), circleIconImage: #imageLiteral(resourceName: "lockedIcon"))
+                alert.showInfo("Unlock All Maps".localized, subTitle: String(format: "Do you want to unlock all locked maps for %@?".localized, price!), circleIconImage: UIImage(named: "lockedicon"))
             } else {
                 self.showIAPError(message: "Unable to get product information. Please check your Internet connection.".localized)
             }
