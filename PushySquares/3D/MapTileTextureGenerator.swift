3026cf1cd77e3bb12186b9d54f2dfd90c5012064
Mulang Su <sumulang@gmail.com>
2021-04-23 10:44:12 +0800

    drew textures for map tiles

--- PushySquares/3D/MapTileTextureGenerator.swift
+++ PushySquares/3D/MapTileTextureGenerator.swift
@@ -6,6 +6,56 @@ enum MapTileTextureGenerator {
     private static var mapTileMaterialCache: [MapTile: SCNMaterial] = [:]
     private static var colorMaterialCache: [UIColor: SCNMaterial] = [:]
 
+    static func material(for mapTile: MapTile) -> SCNMaterial? {
+        if let cached = mapTileMaterialCache[mapTile] {
+            return cached
+        }
+
+        if mapTile == .void {
+            return nil
+        }
+
+        if mapTile == .slippery {
+            let material = SCNMaterial()
+            material.diffuse.contents = UIImage(named: "wet")
+            mapTileMaterialCache[mapTile] = material
+            return material
+        }
+
+        let size = 500.f
+        UIGraphicsBeginImageContext(CGSize(width: size, height: size))
+        defer { UIGraphicsEndImageContext() }
+
+        UIColor(hex: "fff4d0").setFill()
+        UIRectFill(CGRect(x: 0, y: 0, width: size, height: size))
+
+        if mapTile != .wall {
+            drawSimpleStripes(x: 0, y: 0, width: size, height: size, strokeWidth: size / 8)
+        }
+
+        let strokeColor: UIColor
+        if mapTile == .ground || mapTile == .wall{
+            strokeColor = .black
+        } else if case .spawnpoint(let color) = mapTile {
+            strokeColor = BoardView.colorToUIColor[color]!
+        } else {
+            fatalError("This shouldn't happen!")
+        }
+
+        strokeColor.setStroke()
+        let border = UIBezierPath(rect: CGRect(x: 0, y: 0, width: size, height: size))
+        border.lineWidth = size / 8
+        border.stroke()
+
+        let image = UIGraphicsGetImageFromCurrentImageContext()!.flipImage()!
+
+        let material = SCNMaterial()
+        material.diffuse.contents = image
+
+        mapTileMaterialCache[mapTile] = material
+        return material
+    }
+
 }
 
 extension UIImage {
