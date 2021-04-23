c17afbfc35a3fecbfc0bf45cf10f4c14d16c0c1a
Mulang Su <sumulang@gmail.com>
2021-04-23 10:43:52 +0800

    Create MapTileTextureGenerator.swift

---
+++ PushySquares/3D/MapTileTextureGenerator.swift
@@ -0,0 +1,27 @@
+import UIKit
+import PushySquaresModel
+import SceneKit
+
+enum MapTileTextureGenerator {
+    private static var mapTileMaterialCache: [MapTile: SCNMaterial] = [:]
+    private static var colorMaterialCache: [UIColor: SCNMaterial] = [:]
+
+}
+
+extension UIImage {
+    func flipImage() -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
+        let bitmap = UIGraphicsGetCurrentContext()!
+
+        bitmap.translateBy(x: size.width / 2, y: size.height / 2)
+        bitmap.scaleBy(x: -1.0, y: -1.0)
+
+        bitmap.translateBy(x: -size.width / 2, y: -size.height / 2)
+        bitmap.draw(self.cgImage!, in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return image
+    }
+}
