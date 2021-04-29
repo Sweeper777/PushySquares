e4a4500b672b9f976d26d5061a0051bb669b716d
Mulang Su <sumulang@gmail.com>
2021-04-29 10:22:52 +0800

    added Camera.swift

---
+++ PushySquares/3D/Camera.swift
@@ -0,0 +1,19 @@
+import SceneKit
+
+class HorizontalPivotCamera {
+    private let target: SCNNode
+    private let cameraNode: SCNNode
+    private let radius: Float
+    private var angle: CGFloat = 0
+
+    init(target: SCNNode, cameraNode: SCNNode) {
+        self.target = target
+        self.cameraNode = cameraNode
+        radius = hypotf(target.position.x - cameraNode.position.x, target.position.z - cameraNode.position.z)
+        let lookAtConstraint = SCNLookAtConstraint(target: target)
+        lookAtConstraint.isGimbalLockEnabled = true
+        if (cameraNode.constraints?.append(lookAtConstraint)) == nil {
+            cameraNode.constraints = [lookAtConstraint]
+        }
+    }
+}
