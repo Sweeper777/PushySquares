577d78fa080a1bb981b0aff6d6c49857106b77f9
Mulang Su <sumulang@gmail.com>
2021-04-30 10:27:18 +0800

    added eulerY

--- PushySquares/3D/Camera.swift
+++ PushySquares/3D/Camera.swift
@@ -4,11 +4,14 @@ class HorizontalPivotCamera {
     private let target: SCNNode
     private let cameraNode: SCNNode
     private let radius: Float
-    private var angle: CGFloat = 0
+    private var angle: Float
+    private var eulerY: Float
 
     init(target: SCNNode, cameraNode: SCNNode) {
         self.target = target
         self.cameraNode = cameraNode
         radius = hypotf(target.position.x - cameraNode.position.x, target.position.z - cameraNode.position.z)
+        angle = atan2f(cameraNode.position.z - target.position.z, cameraNode.position.x - target.position.x)
+        eulerY = cameraNode.eulerAngles.y
     }
 }
