be3298b5369558465bb525a0ea3a46c7500defca
Mulang Su <sumulang@gmail.com>
2021-05-01 14:18:56 +0800

    added SceneAnimationPhase.block(forAnimationType:)

--- PushySquares/Animations/SceneAnimationPhase.swift
+++ PushySquares/Animations/SceneAnimationPhase.swift
@@ -9,6 +9,21 @@ class SceneAnimationPhase : AnimationPhase {
     func start(animations: [AnimationType: [SCNNode]]) {
     }
 
+    func block(forAnimationType animationType: AnimationType) -> (SCNNode) -> Void {
+        switch animationType {
+        case .move(dx: let dx, dy: let dy):
+            return { node in
+                node.position.x += Float(dx)
+                node.position.z += Float(dy)
+            }
+        case .fall:
+            return { $0.position.y -= 10 }
+        case .grayOut:
+            return { $0.geometry?.firstMaterial?.diffuse.contents = UIColor.gray }
+        case .newSquare:
+            return { $0.scale = SCNVector3(1, 1, 1) }
+        }
+    }
 
     required init(duration: TimeInterval) {
         self.duration = duration
